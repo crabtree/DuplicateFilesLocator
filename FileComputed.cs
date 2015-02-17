0f89c17b64b5a0e58fd5628f4f808773c2569ab5
Krystian Cieslik <krystian.cieslik@o2.pl>
2015-02-17 12:45:34 +0100

    no message

---
+++ FileComputed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateFilesLocator
+{
+    public class FileComputed
+    {
+        public int Amount { get; set; }
+
+        public string Hash { get; set; }
+
+        public List<string> Paths { get; set; }
+
+        public FileComputed(string hash, string path)
+        {
+            this.Amount = 1;
+            this.Hash = hash;
+            this.Paths = new List<string>();
+            this.Paths.Add(path);
+        }
+
+        public void AddItem(string path)
+        {
+            this.Amount += 1;
+            this.Paths.Add(path);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace DuplicateFilesLocator
+{
+    class Program
+    {
+        private static Dictionary<string, FileComputed> FilesCounter;
+
+        static void Main(string[] args)
+        {
+            string startPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            if (args.Length > 0 && !args[0].Equals("."))
+            {
+                startPath = args[0];
+            }
+
+            Program.FilesCounter = new Dictionary<string, FileComputed>();
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(startPath);
+
+            PerformDirectoryAction(rootDirectory);
+
+            if (Program.FilesCounter.Any(f => f.Value.Amount > 1))
+            {
+                foreach (var item in Program.FilesCounter)
+                {
+                    if (item.Value.Amount > 1)
+                    {
+                        Console.WriteLine(String.Format("Duplicate found: {0}", item.Value.Amount));
+                        foreach (var path in item.Value.Paths)
+                        {
+                            Console.WriteLine(String.Format("File: {0}", path));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("No duplicates found");
+            }
+
+            Console.WriteLine("Press ENTER to end");
+            Console.ReadLine();
+        }
+
+        private static void PerformDirectoryAction(IEnumerable<DirectoryInfo> directories)
+        {
+            foreach (var directory in directories)
+            {
+                PerformDirectoryAction(directory);
+            }
+        }
+
+        private static void PerformDirectoryAction(DirectoryInfo directory)
+        {
+            if (!directory.Exists) return;
+
+            var innerDirectories = directory.GetDirectories();
+            PerformDirectoryAction(innerDirectories);
+
+            var innerFiles = directory.GetFiles();
+            PerformFileAction(innerFiles);
+        }
+
+        private static void PerformFileAction(IEnumerable<FileInfo> files)
+        {
+            foreach (var file in files)
+            {
+                PerformFileAction(file);
+            }
+        }
+
+        private static void PerformFileAction(FileInfo file)
+        {
+            if (!file.Exists) return;
+
+            var fileBytes = GetFileBytes(file);
+            var fileHash = ComputeFileHash(fileBytes);
+
+            if (Program.FilesCounter.ContainsKey(fileHash))
+            {
+                Program.FilesCounter[fileHash].AddItem(file.FullName);
+            }
+            else
+            {
+                Program.FilesCounter[fileHash] = new FileComputed(fileHash, file.FullName);
+            }
+        }
+
+        private static byte[] GetFileBytes(FileInfo file)
+        {
+            byte[] fileBytes = new byte[file.Length];
+            using (FileStream stream = file.OpenRead())
+            {
+                stream.Read(fileBytes, 0, fileBytes.Length);
+                stream.Close();
+            }
+            return fileBytes;
+        }
+
+        private static string ComputeFileHash(byte[] fileBytes)
+        {
+            var sha1 = new SHA1CryptoServiceProvider();
+            var ret = sha1.ComputeHash(fileBytes);
+            return BitConverter.ToString(ret);
+        }
+    }
+}
